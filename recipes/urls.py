de254ad722ce992b24f0477b0cb7d6ece2ae4607
Anaê Ross <anae08@gmail.com>
2023-07-10 17:04:53 -0300

    Adicionado models / migrations

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 
 from . import views
 
+# recipes:recipe
+app_name = 'recipes'
+
 urlpatterns = [
-    path('home/', views.index, name="index"),
-    path('about/', views.about, name="about"),
-    path('contact/', views.contact, name="contact"),
+    # path('home/', views.index, name="index"),
+    # path('about/', views.about, name="about"),
+    # path('contact/', views.contact, name="contact"),
+    path('', views.index, name="index"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,17 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+
+from .utils.recipes.factory import make_recipe
 
 
 def index(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Anaê Ross',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
-def about(request):
-    return HttpResponse("This is my about page!")
-
-
-def contact(request):
-    return HttpResponse("This is my contact page, send us an email")
-
-
-
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
